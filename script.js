78411b2a5de7325ef2d4fd23cd80069a226e1a98
entangled-roots <tangled.roots.holly@gmail.com>
2025-01-03 15:18:37 +0000

    added marker images

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r
 
 // Add a marker (existing functionality)
 L.marker([51.4545, -2.5879]).addTo(map)
-    .bindPopup('Howdy !<br> Community spot.')
+    .bindPopup('Howdy default!<br> Community spot.')
     .openPopup();
 
 // Define custom icons for categories
