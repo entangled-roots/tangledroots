d673c870de563262677b315170f7ae99fe84bdc3
entangled-roots <tangled.roots.holly@gmail.com>
2025-01-03 11:46:12 +0000

    changed tiles and default starting point

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 // Initialize the map
-const map = L.map('map').setView([51.505, -0.09], 13); // Centered on London, UK
+const map = L.map('map').setView([51.4545, -2.5879], 13); // Centered on London, UK
 
-// Add OpenStreetMap tiles
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    maxZoom: 19
+
+
+L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
 }).addTo(map);
+// Add OpenStreetMap tiles
+//L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//    maxZoom: 19
+//}).addTo(map);
 
 // Add a marker
 L.marker([51.505, -0.09]).addTo(map)
